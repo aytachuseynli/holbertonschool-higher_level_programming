3404e0fc95e6d55db727e611695508e88bbf04f9
aytachuseynli <aytac_ha@mail.ru>
2024-01-30 10:06:11 -0800

    uniqie elements

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_elements = list(set(my_list))
+    unique_elements = []
+    for item in my_list:
+        if item not in unique_elements:
+            unique_elements.append(item)
     return unique_elements
