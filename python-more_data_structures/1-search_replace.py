e0f3e3cef2b7f83f5d13aaf667c728685543bad7
aytachuseynli <aytac_ha@mail.ru>
2024-01-30 09:54:24 -0800

    new

--- python-more_data_structures/1-search_replace.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-
-    return(my_list)
--- python-more_data_structures/2-uniq_add.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-   unique_elements = list(set(my_list))
-   return unique_elements
