9c8d00d4dbc13eb56cf41cba4c03d2f9861b7916
Aytac Huseynli <aytac_ha@mail.ru>
2024-01-30 21:39:16 +0400

    new

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
+
+    print(my_list)
