88d5edad315d9a20d2a2068077642c3cbad194cf
aytachuseynli <aytac_ha@mail.ru>
2024-02-02 00:36:03 -0800

    yes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The size of the square (private).
+        """
         self.__size = size
