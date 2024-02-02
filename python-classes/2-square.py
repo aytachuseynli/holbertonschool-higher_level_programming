73ddc90cc725d9d96a07f733943ce23a8ab646ab
aytachuseynli <aytac_ha@mail.ru>
2024-02-02 00:37:22 -0800

    it seems that I can do

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
+"""
+Define a class Square with size validation.
+"""
+
 class Square:
+    """
+    Represents a square with size validation.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+        """
         self.__size = size
         self.__validate_size()
 
     def __validate_size(self):
+        """
+        Validates the size attribute.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
