6516f5bedb1ee79b5cdd34a63be789291bf9e79f
aytachuseynli <aytac_ha@mail.ru>
2024-02-02 01:03:04 -0800

    good job

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,45 +1,83 @@
 #!/usr/bin/python3
+"""
+Define a class Square with size validation and printing methods.
+"""
+
+
 class Square:
-    def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+    """
+    Represents a square with size validation and printing methods.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+        """
+        self.__size = size
 
     @property
     def size(self):
+        """
+        Getter method for the size attribute.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter method for the size attribute.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         self.__validate_size(value)
         self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        self.__validate_position(value)
-        self.__position = value
-
     def __validate_size(self, value):
+        """
+        Validates the size attribute.
+
+        Args:
+            value (int): The size to be validated.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
 
-    def __validate_position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Prints the square with the character '#' to stdout.
+        If size is 0, prints an empty line.
+        """
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
-                print()
             for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                print("#" * self.__size)
