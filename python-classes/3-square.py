a01ccd10b5d612b2d14232841349c19647b7bcc2
aytachuseynli <aytac_ha@mail.ru>
2024-02-02 00:49:52 -0800

    yuppi

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python3
+"""
+Define a class Square with size validation.
+"""
+
 class Square:
+    """
+    Represents a square with size validation.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+        """
         self.__size = size
         self.__validate_size()
 
     def __validate_size(self):
+        """
+        Validates the size attribute.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
