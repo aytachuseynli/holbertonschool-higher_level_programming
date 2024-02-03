7f78fc92945aeae35df5111e4b21dd178944499c
aytachuseynli <aytac_ha@mail.ru>
2024-02-03 10:10:06 -0800

    yep

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,10 @@
 def safe_print_division(a, b):
     try:
         result = a/b
+        print("Inside result: {}\n{} / {} = {}".format(result, a, b, result))
     except (ZeroDivisionError, TypeError) as e:
         print("An error occurred:", e)
         return None
-    else:
-        print("Inside result: {}".format(result))
-        return result
     finally:
         print("Finally block: Division operation completed.")
+    return result
