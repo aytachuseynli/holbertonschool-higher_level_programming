d429138cfb88d87a428b827846eb926d807897f7
aytachuseynli <aytac_ha@mail.ru>
2024-02-03 10:36:19 -0800

    be clever

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    
+
     for i in range(list_length):
         try:
             # Try to perform the division
             element_1 = my_list_1[i] if i < len(my_list_1) else 0
-            element_2 = my_list_2[i] if i < len(my_list_2) else 1  # Avoid division by zero
+            element_2 = my_list_2[i] if i < len(my_list_2) and my_list_2[i] != 0 else 1  # Avoid division by zero
             result = element_1 / element_2
+            result_list.append(result)
         except ZeroDivisionError:
             print("division by 0")
             result_list.append(0)
@@ -17,9 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result_list.append(0)
-        else:
-            result_list.append(result)
         finally:
-            pass
-    
+            pass  # Optionally, you can include code here that should execute regardless of exceptions
+
     return result_list
