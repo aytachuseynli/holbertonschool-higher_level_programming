a0d9af573aea7dd719d25fc916c9bf754347d5d4
aytachuseynli <aytac_ha@mail.ru>
2024-02-03 10:47:04 -0800

    you have a message

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
+
     for i in range(list_length):
         try:
-            result = my_list[i] / my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
             result = 0
@@ -15,4 +17,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
+
     return result_list
