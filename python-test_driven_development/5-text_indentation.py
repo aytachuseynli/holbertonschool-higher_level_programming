c0f3a2bc7eaef7393cabced8db50d2348462e299
aytachuseynli <aytac_ha@mail.ru>
2024-02-04 09:12:41 -0800

    text identation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+  Text indentation
+"""
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    separators = ['.', '?', ':']
+    lines = []
+
+    current_line = ''
+    for char in text:
+        current_line += char
+        if char in separators:
+            lines.append(current_line.strip())
+            lines.append('\n')
+            current_line = ''
+
+    if current_line:
+        lines.append(current_line.strip())
+
+    print(''.join(lines))
