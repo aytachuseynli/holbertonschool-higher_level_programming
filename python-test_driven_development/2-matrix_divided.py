a93f7b633e7e0ed400d46975677e1dbeb9913a17
aytachuseynli <aytac_ha@mail.ru>
2024-02-04 07:25:34 -0800

    bj

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,30 +7,22 @@
 def matrix_divided(matrix, div):
     """
     Divide all elements of a matrix by a given number.
-
-    Args:
-        matrix (list of lists): Matrix of integers or floats.
-        div (int or float): The divisor.
-
-    Returns:
-        list of lists: New matrix with elements divided by div, rounded to 2 decimal places.
-
-    Raises:
-        TypeError: If matrix is not a list of lists of integers/floats,
-                   or if div is not a number (integer or float).
-        TypeError: If each row of the matrix doesn't have the same size.
-        ZeroDivisionError: If div is equal to 0.
     """
-    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if len(set(len(row) for row in matrix)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
-
+    if not isinstance(matrix, list) or\
+       not any(isinstance(matrix[0], list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        for elem in row:
+            if not isinstance(elem, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix " +
+                    "(list of lists) of integers/floats")
+    for row in matrix:
+        if len(row) is not len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    if div is 0:
         raise ZeroDivisionError("division by zero")
-
-    return [[round(num / div, 2) for num in row] for row in matrix]
+    return [[round(elem / div, 2) for elem in row] for row in matrix]
