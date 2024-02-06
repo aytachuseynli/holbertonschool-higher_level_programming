edd8f9a21b1d27a7fe2f8b0f26e1389d4a43fbbc
aytachuseynli <aytac_ha@mail.ru>
2024-02-05 22:42:07 -0800

    define rectangle properties

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@ Define rectangle properties
 class Rectangle:
 
     def __init__(self, width=0, height=0):
-        
+
         self._width = 0
         self._height = 0
         self.width = width
@@ -19,7 +19,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
- 
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,10 +32,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-      
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self._height = value
-
