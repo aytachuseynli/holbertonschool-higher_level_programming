bf37d340207da13e76859de82a21e19cf9837cc1
aytachuseynli <aytac_ha@mail.ru>
2024-02-08 01:58:34 -0800

    work

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -43,4 +43,5 @@ class Rectangle:
 
     def perimeter(self):
         """Rectangle perimeter"""
-        return 2 * (self.width + self.height) if self.width != 0 and self.height != 0 else 0
+        return (2 * (self.width + self.height) 
+            if self.width != 0 and self.height != 0 else 0)
