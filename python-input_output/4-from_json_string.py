c66ebbe8b15f0c6945841919b514b71eeac3b5ea
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 12:35:08 -0800

    From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+From JSON string to Object
+"""
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON-formatted string to a Python object.
+    """
+
+    return json.loads(my_str)
+
