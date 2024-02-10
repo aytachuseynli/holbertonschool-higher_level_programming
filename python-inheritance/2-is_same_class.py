f0aa84df4861a8c8ae135ef4b2b221e348fa273f
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 11:06:58 -0800

    same class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module  2-is_same_class.py
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+    """
+
+    return type(obj) is a_class
