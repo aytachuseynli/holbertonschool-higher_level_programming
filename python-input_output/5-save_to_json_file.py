482f5da05f3632788cb334d41fd402591ccef9e5
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 12:44:37 -0800

    Thunder, thunder, feel the thunder

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Save Object to a file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file 
+    using a JSON representation.
+    """
+
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
