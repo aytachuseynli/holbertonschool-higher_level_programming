9ca544c3de209e9b4c03d8ac99a1bd2b54417ab0
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 13:49:05 -0800

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Student to JSON with filter
+"""
+
+
+class Student:
+    """
+    Student class with enhanced to_json method.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
