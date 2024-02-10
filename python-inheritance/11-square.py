ad7474f1aaa498265bc2e91fc525f52801593b44
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 15:28:51 -0800

    n

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module 11-square.py
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+
+class Square(Rectangle):
+    """
+    Square class inheriting from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square instance
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns the square description
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    s = Square(4)
+    print(s)
+    print("Area:", s.area())
