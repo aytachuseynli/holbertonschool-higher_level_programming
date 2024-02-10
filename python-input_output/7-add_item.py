6f9304601065e9c5fb6046708a3493dbf0b38a04
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 13:12:29 -0800

    load, add,save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,7 +13,8 @@ from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 def add_items_to_list_and_save(filename, *items):
-    """save add load"""
+   #save add load
+
     try:
         existing_data = load_from_json_file(filename)
     except (FileNotFoundError, json.JSONDecodeError):
@@ -23,8 +24,14 @@ def add_items_to_list_and_save(filename, *items):
     save_to_json_file(existing_data, filename)
 
 if __name__ == "__main__":
+    # Default filename if not provided as an argument 
     default_filename = "add_item.json"
+
+    # Use the provided filename or the default one
     filename = sys.argv[1] if len(sys.argv) > 1 else default_filename
+
+    # Extract items from command-line arguments
     items = sys.argv[2:]
 
+    # Add items to the list and save to the file
     add_items_to_list_and_save(filename, *items)
