a2893fa7051a902dc222d9e3320d5c1fde3de4aa
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 13:06:21 -0800

    another python task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,15 +9,22 @@ import sys
 import os
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-filename = "add_item_json"
+def add_items_to_list_and_save(filename, *items):
+    """save add load"""
+    try:
+        existing_data = load_from_json_file(filename)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = []
 
-if os.path.isfile(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
+    existing_data.extend(items)
+    save_to_json_file(existing_data, filename)
 
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    default_filename = "add_item.json"
+    filename = sys.argv[1] if len(sys.argv) > 1 else default_filename
+    items = sys.argv[2:]
+
+    add_items_to_list_and_save(filename, *items)
