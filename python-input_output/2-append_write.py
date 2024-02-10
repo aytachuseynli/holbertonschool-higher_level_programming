de8b5bebb80e005370c0031eb4e6d188d47d93dd
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 12:32:23 -0800

    to json

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ Append to a file
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file (UTF8) and 
+    Appends a string at the end of a text file (UTF8) and
     returns the number of characters added.
     """
 
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+To JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Converts a Python object to its JSON representation.
+    """
+    return json.dumps(my_obj)
