b24343bb78570415e046217cd7ce08e0054c0b5b
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 13:59:02 -0800

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Student to JSON with filter
+"""
+
+
+class Student:
+    """
+    Student class with enhanced to_json method.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key)
+                    for key in attrs
+                    if hasattr(self, key)}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        based on the provided dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
