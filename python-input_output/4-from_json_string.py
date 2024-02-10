3e67fe0bf626970e2ca6c830ee88ed87b56f3f6c
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 12:39:35 -0800

    Pythonnnnnnnnnnnnnnnn, why do you tire me out?

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,12 +7,9 @@ From JSON string to Object
 import json
 
 
-
-
 def from_json_string(my_str):
     """
     Converts a JSON-formatted string to a Python object.
     """
 
     return json.loads(my_str)
-
