abb2ce4c894d4d4edd0e0304a86d16896e885abf
aytachuseynli <aytac_ha@mail.ru>
2024-02-10 10:58:43 -0800

    sort

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-print ascending sorted list
+Print ascending sorted list
 """
-
 
 class MyList(list):
     """
-    MyList is a subclass of the built list class
+    MyList is a subclass of the built-in list class
     with an additional method.
     """
 
     def print_sorted(self):
-    """
-    Sorts the list in ascending order using 
-    the built-in sort method and prints 
-    the sorted list.
-    """
-
-    print(sorted(self))
+        """
+        Sorts the list in ascending order using 
+        the built-in sort method and prints 
+        the sorted list.
+        """
+        print(sorted(self))
