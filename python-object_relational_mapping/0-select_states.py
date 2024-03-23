4a03995d6cb6a5ea71fd05c7940315f80fee458a
aytachuseynli <aytac_ha@mail.ru>
2024-03-23 19:15:05 +0000

    select state

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,23 +4,15 @@
 import MySQLdb
 import sys
 
-def main():
-    def list_states(username, password, db_name):
-        db = MySQLdb.connect(host="localhost",
-                             user=username,
-                             passwd=password,
-                             db=db_name,
-                             port=3306)
-
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-        cursor.close()
-        db.close()
-
-
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(
+        user=sys.argv[1], 
+        passwd=sys.argv[2], 
+        db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
