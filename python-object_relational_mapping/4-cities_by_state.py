ca4c8ec5baf211fa54368f33810ff169ddb4464a
aytachuseynli <aytac_ha@mail.ru>
2024-03-23 22:21:42 +0000

    lists all cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,3 +2,22 @@
 """
  lists all cities
 """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_cities.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+
+    [print(row) for row in cursor.fetchall()]
+
+    db.close()
