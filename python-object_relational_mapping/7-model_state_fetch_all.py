2536a69378e8322b4e47464e4e8b9535f2c0afd0
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:24:33 +0000

    model

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,3 +4,16 @@ All states via SQLAlchemy
 """
 
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Class representing a state.
+    """
+    
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
