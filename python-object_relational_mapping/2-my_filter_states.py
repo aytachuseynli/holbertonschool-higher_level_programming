3a904c2b191fcd7e407124ed951bf9376fa1df4c
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 20:01:13 +0000

    new

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values 
+takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-
-
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", 
-                           port=3306, 
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
                            user=argv[1],
-                           passwd=argv[2], 
-                           db=argv[3], 
+                           passwd=argv[2],
+                           db=argv[3],
                            charset="utf8")
     cur = conn.cursor()
     query = """
-               SELECT * 
-               FROM states 
-               WHERE name 
-               LIKE BINARY '{}' 
-               ORDER BY states.id ASC"""
+        SELECT *
+        FROM states
+        WHERE name
+        LIKE BINARY '{}'
+        ORDER BY states.id ASC"""
     query = query.format(argv[4])
     cur.execute(query)
     query_rows = cur.fetchall()
