d3359cc294762b29c49d5088ade362eb88bf13c8
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:37:42 +0000

    modified:

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a script that lists all State objects that 
+Write a script that lists all State objects that
 contain the letter a from the database hbtn_0e_6_usa
 """
 import sys
@@ -17,12 +17,12 @@ if __name__ == "__main__":
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True)
-    
+
     Base.metadata.create_all(engine)
-    
+
     session = Session(engine)
-    
+
     for state in session.query(State).filter(State.name.contains("a")):
         print("{}: {}".format(state.id, state.name))
-    
+
     session.close()
