ede00c814066361982b6f99eb7448c82eda8989c
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:34:57 +0000

    modified:

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,7 +3,6 @@
 Write a script that lists all State objects that 
 contain the letter a from the database hbtn_0e_6_usa
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
