8dcfd38988877d629ce8f65ac18b989890dc3039
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:13:29 +0000

    safe filter

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,29 +10,19 @@ import sys
 
 
 if __name__ == "__main__":
-    argv = sys.argv
-    username = argv[1]
-    password = argv[2]
-    db = argv[3]
-    name = argv[4]
-
     conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db,
-        charset="utf8"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
-
     cur = conn.cursor()
-    cur.execute("""
-                SELECT *
-                FROM states
-                WHERE name 
-                LIKE BINARY %s
-                ORDER BY id ASC
-                """.format(name))
+
+    cur.execute(
+        """
+                SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id
+                """,
+        (sys.argv[4],),
+    )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
