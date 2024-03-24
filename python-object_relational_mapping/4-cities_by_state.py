17bce24c9ebb1c15c7292f409a85e115b9b9f87c
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:23:56 +0000

    cities by state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,19 +2,18 @@
 """
 Lists all cities
 """
-
-
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", 
-                           port=3306, 
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
                            user=argv[1],
-                           passwd=argv[2], 
-                           db=argv[3], 
+                           passwd=argv[2],
+                           db=argv[3],
                            charset="utf8")
+
     cur = conn.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "JOIN states ON cities.state_id = states.id "
@@ -22,5 +21,6 @@ if __name__ == "__main__":
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
