22c916e9f4d7c71bdf3e326f495f2513808c5bfc
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:54:44 +0000

    mod

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Write a script that deletes all State objects with a name containing the letter a
+Write a script that deletes all State
+objects with a name containing the letter a
 """
-
-
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine(
