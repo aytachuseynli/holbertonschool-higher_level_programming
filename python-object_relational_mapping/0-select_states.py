0caae3177b00b1007119cf53c97e7dc3fedd030c
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 11:49:25 +0000

    filter states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,17 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py <username> <password> <database>")
-        sys.exit(1)
-
-    username, password, database = sys.argv[1:4]
+    argv = sys.argv
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
 
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database,
+        db=db,
         charset="utf8"
     )
     cur = conn.cursor()
