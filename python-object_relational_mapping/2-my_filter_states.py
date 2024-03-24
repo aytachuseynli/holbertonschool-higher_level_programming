bc8b85501b07112bdaa43914d2687d2eef9a8213
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 12:17:57 +0000

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in an argument and displays all values 
+takes in an argument and displays all values 
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
@@ -8,31 +8,28 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    name = sys.argv[4]
+    argv = sys.argv
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+    name = argv[4]
 
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database,
-        charset="utf8"
-    )
+        db=db,
+        charset="utf8")
+    
     cur = conn.cursor()
-
     cur.execute("""
-                SELECT * FROM states WHERE name = %s ORDER BY id ASC
-                """, (name,))
+                SELECT * FROM states WHERE name = "{:s}" ORDER BY id ASC
+                """.format(name))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
-
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     conn.close()
+    
