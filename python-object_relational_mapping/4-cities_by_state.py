b8b6fc6acb667d377edf5b1d69665885901b2248
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 17:27:01 +0000

    cities  by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -5,34 +5,21 @@ Lists all cities
 
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    argv = sys.argv
-    username = argv[1]
-    password = argv[2]
-    db = argv[3]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db,
-        charset="utf8"
-    )
-
+    conn = MySQLdb.connect(host="localhost", 
+                           port=3306, 
+                           user=argv[1],
+                           passwd=argv[2], 
+                           db=argv[3], 
+                           charset="utf8")
     cur = conn.cursor()
-    cur.execute("""
-                SELECT cities.id, cities.name, states.name
-                FROM cities
-                INNER JOIN states
-                ON cities.state_id=states.id
-                ORDER BY cities.id ASC
-                """)
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
-
     cur.close()
     conn.close()
