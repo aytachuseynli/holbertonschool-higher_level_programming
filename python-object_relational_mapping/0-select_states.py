697ecf6b260425e747d9cb0e211dbf3ec18d2731
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 11:38:11 +0000

    filter states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,6 @@ import sys
 import sqlalchemy
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <username> <password> <database>")
-        sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -23,13 +20,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     sql = "SELECT * FROM states ORDER BY id ASC"
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
             print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    finally:
-        db.close()
+    cursor.close()
+    db.close()
