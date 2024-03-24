9f68a3bdfc4951c4ccb66e18a2e6fb714281f9a8
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 11:46:47 +0000

    filter states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,27 +2,29 @@
 """lists all states """
 
 import MySQLdb
-import sqlalchemy
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database>")
+        sys.exit(1)
 
+    username, password, database = sys.argv[1:4]
 
-conn = MySQLdb.connect(
-        host="localhost", 
-        port=3306, 
-        user=username, 
-        passwd=password, 
-        db=database, 
-        charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
