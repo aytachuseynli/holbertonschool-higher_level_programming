8ecebef47984e9266f297227b2a66d16cc0adb83
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 11:18:27 +0000

    filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,6 @@ import sqlalchemy
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py <username> <password> <database>")
-        sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -25,15 +22,10 @@ if __name__ == "__main__":
     cursor = db.cursor()
     sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
 
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
+    cursor.execute(sql)
+    results = cursor.fetchall()
 
-        for row in results:
-            print(row)
+    for row in results:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        
-    finally:
-        db.close()
+    db.close()
