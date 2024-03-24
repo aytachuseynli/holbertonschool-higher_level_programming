7a9b4c973286dbab33d03a7f968ca4dab5c06649
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:40:32 +0000

    modified

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a script that prints the State object with 
+Write a script that prints the State object with
 the name passed as argument from the database hbtn_0e_6_usa
 """
 
@@ -14,17 +14,17 @@ if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states = session.query(State).\
         filter(State.name == argv[4]).order_by(State.id).all()
-    
+
     if states:
         print("{}".format(states[0].id))
     else:
         print("Not found")
-    
+
     session.close()
