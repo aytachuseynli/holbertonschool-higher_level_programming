dbd48e4d95c4af91ffe3f16dcbc9078fbe791113
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:42:23 +0000

    model state insert

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+Write a script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
 """
-
-
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine(
