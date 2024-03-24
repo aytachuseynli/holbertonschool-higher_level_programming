bdb4755f33e8491a2f53ffcb38c3881a3fb0a3c1
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 19:48:26 +0000

    filter:

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,19 +2,18 @@
 """
 Cities in state
 """
-
-
 from sys import argv
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
