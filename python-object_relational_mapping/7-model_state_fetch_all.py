6ddda9e6f3d73efdfe992c23adf58894091cf8a1
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:58:30 +0000

    model

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ All states via SQLAlchemy
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
     """
