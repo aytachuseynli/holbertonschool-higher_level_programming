29d67cc6aa04014d2e9559941007a186b8dc72b7
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:20:12 +0000

    modify

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,12 +9,18 @@ import sys
 
 
 if __name__ == "__main__":
-    argv = sys.argv
-    username = argv[1]
-    password = argv[2]
-    db = argv[3]
-    name = argv[4]
+    # Check if correct number of arguments provided
+    if len(sys.argv) != 5:
+        print("Usage: ./3-my_safe_filter_states.py <username> <password> <database> <state_name>")
+        sys.exit(1)
 
+    # Extract arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4]
+
+    # Connect to MySQL database
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -24,17 +30,22 @@ if __name__ == "__main__":
         charset="utf8"
     )
 
+    # Create cursor
     cur = conn.cursor()
-    cur.execute("""
-                SELECT *
-                FROM states
-                WHERE name 
-                LIKE BINARY %s
-                ORDER BY id ASC
-                """, (name ,))
+
+    # Execute safe parameterized query
+    cur.execute(
+        """
+        SELECT * FROM states WHERE name = %s ORDER BY id ASC
+        """,
+        (name,)
+    )
+
+    # Fetch and print results
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
 
+    # Close cursor and connection
     cur.close()
     conn.close()
