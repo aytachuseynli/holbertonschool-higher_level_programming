4ad8d8fbaa50c219e7a2d1b4a02a7045300a02d9
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:04:13 +0000

    safe filter states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,3 +3,37 @@
  takes in arguments and displays all values in the states table 
  of hbtn_0e_0_usa where name matches the argument.
 """
+
+import MySQLdb
+import sqlalchemy
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+    name = argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db,
+        charset="utf8"
+    )
+
+    cur = conn.cursor()
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name = %s
+                ORDER BY id ASC
+                """.format(name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
