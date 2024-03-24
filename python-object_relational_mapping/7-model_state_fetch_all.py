4902f5e97fbc477c3c26538ed971d4d4982c55cc
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:37:14 +0000

    model

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,6 +11,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    Model
+    """
+    
     username = sys.argv[1]
     password = sys.argv[2]
     db = sys.argv[3]
