6dd26e3808316facf79a2c6ac7315ded086d9e3a
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:17:26 +0000

    modify

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,25 +4,34 @@
  of hbtn_0e_0_usa where name matches the argument.
 """
 
-
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-    )
-    cur = conn.cursor()
+    argv = sys.argv
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+    name = argv[4]
 
-    cur.execute(
-        """
-                SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id
-                """,
-        (sys.argv[4],),
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db,
+        charset="utf8"
     )
+
+    cur = conn.cursor()
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name 
+                LIKE BINARY %s
+                ORDER BY id ASC
+                """, (name ,))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
