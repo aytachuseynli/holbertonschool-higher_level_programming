4d48f5283af13a2b161614baa5065a2dbca5635e
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 11:02:01 +0000

    filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,3 +2,17 @@
 """
  lists all states with a name starting with N (upper N)
 """
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1:4]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor =  db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    [print(row) for row in cursor.fetchall()]
+    db.close
