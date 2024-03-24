cca212fabf7fcb527232f567db6e355774d8cb95
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 12:09:19 +0000

    filter state by user input:

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,22 +8,26 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
+    if len(sys.argv) != 5:
+        print("Usage: ./script.py <username> <password> <database> <state_name>")
+        sys.exit(1)
 
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(sql, (sys.argv[4],))
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
-    
