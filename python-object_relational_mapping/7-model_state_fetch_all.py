8e8d952f1d78ae9b000e961278c63b3578ddd527
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 16:40:25 +0000

    model

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,14 +19,15 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        username, password, db), 
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db), 
         pool_pre_ping=True)
-    Base.metadata.create_all(bind = engine)
+    Base.metadata.create_all(engine)
 
     session = Session(engine)
 
     for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
