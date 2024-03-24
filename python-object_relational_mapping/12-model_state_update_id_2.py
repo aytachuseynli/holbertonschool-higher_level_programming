2feb79547d720195a8b323d970366979db4b251b
aytachuseynli <aytac_ha@mail.ru>
2024-03-24 17:40:08 +0000

    model state fetch first

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,12 +4,14 @@ Write a script that changes the name of a State object from the database hbtn_0e
 """
 
 
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2],
